272c991ffaf5ce595ac3eef427a8dab59dcae2b6
Alexei Scheglov <aleshascheglov@yandex.ru>
2022-11-24 19:23:00 +0300

    fix: исправил footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
       <footer className="footer">
-        <p className="footer__project">{new Date().getFullYear()}</p>
+        <p className="footer__project">© {new Date().getFullYear()} Mesto Russia</p>
       </footer>
     );
 }
